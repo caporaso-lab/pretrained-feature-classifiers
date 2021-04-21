f1f608071cbda48b01e636c1e61193966fbb0cf4
Ben Kaehler <b.kaehler@adfa.edu.au>
2021-04-21 15:15:05 +1000

    added weighted classifier training

--- get_gg.sh
+++ get_gg.sh
@@ -4,6 +4,8 @@ set -e
 
 seqs="inputs/${CLASSIFIER_NAME}-seqs.qza"
 tax="inputs/${CLASSIFIER_NAME}-tax.qza"
+weights="inputs/${CLASSIFIER_NAME}-weights.qza"
+weights_515_806="inputs/${CLASSIFIER_NAME}-weights-515-806.qza"
 log_path="outputs/logs/%j_%x.txt"
 
 SOURCE_SEQS=gg_13_8_otus/rep_set/99_otus.fasta
@@ -11,6 +13,10 @@ SOURCE_TAX=gg_13_8_otus/taxonomy/99_otu_taxonomy.txt
 
 wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz
 tar -zxf gg_13_8_otus.tar.gz
+wget https://github.com/BenKaehler/readytowear/blob/master/data/gg_13_8/515f-806r/average.qza \
+    -O "${weights_515_806}"
+wget https://github.com/BenKaehler/readytowear/blob/master/data/gg_13_8/full_length/average.qza \
+    -O "${weights}"
 
 # Import
 job_import_seqs=$(
